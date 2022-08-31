06fed02a8d457f59ed20af1085ccb57b316b7514
arbertrary <endzeit7@gmail.com>
2022-08-31 10:58:21 +0200

    added dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-mindmap';
+  private darkThemeIcon = 'nightlight_round';
+  private lightThemeIcon = 'wb_sunny';
+  public lightDarkToggleIcon = this.darkThemeIcon;
+  lightToggled = false;
+
+  toggleLightTheme(): void {
+    document.body.classList.toggle('light-theme');
+    this.lightDarkToggleIcon = this.lightToggled ? this.darkThemeIcon : this.lightThemeIcon;
+    this.lightToggled = !this.lightToggled;
+
+  }
 }
